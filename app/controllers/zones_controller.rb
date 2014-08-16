a28a782a106b1aaf7aae82a6a2a936467f03158a
epelaez1 <enriquepelaez97@gmail.com>
2014-08-16 18:38:57 +0200

    added new zone view and the system to add the zone to the table

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -3,4 +3,25 @@ class ZonesController < ApplicationController
 		@newzone = User.find(current_user.id).zones.new
 
 	end
+	def create
+		if !hasUserThisZone? && paramPermited?
+			addZoneToUser
+		else
+			redirect_to new_zone_path
+			return
+		end
+		binding.pry
+		redirect_to root_path
+	end
+	def hasUserThisZone?
+		User.find(current_user.id).zones.find_by(:zone => params[:zone])
+	end
+	def addZoneToUser
+		@addzone = User.find(current_user.id).zones.new :zone => params[:zone]
+		@addzone.save
+
+	end
+	def paramPermited?
+		params[:zone] == "social"
+	end
 end
