9fb49c4438d00426ae1820867deea9fe1dfd08fc
epelaez1 <enriquepelaez97@gmail.com>
2014-08-16 20:02:36 +0200

    added a view to add zone, with the zones user hasnt

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,27 +1,46 @@
 class ZonesController < ApplicationController
+	
 	def new 
 		@newzone = User.find(current_user.id).zones.new
-
+		@zonesUserCanAdd = zonesUserHasnt
 	end
 	def create
-		if !hasUserThisZone? && paramPermited?
+		if !hasUserThisZone?(params[:zone]) && paramPermited?
 			addZoneToUser
+			redirect_to root_path
+			return
 		else
 			redirect_to new_zone_path
 			return
 		end
-		binding.pry
-		redirect_to root_path
 	end
-	def hasUserThisZone?
-		User.find(current_user.id).zones.find_by(:zone => params[:zone])
-	end
-	def addZoneToUser
-		@addzone = User.find(current_user.id).zones.new :zone => params[:zone]
-		@addzone.save
 
+	private
+	def hasUserThisZone?(zone)
+		User.find(current_user.id).zones.find_by(:zone => zone)
 	end
+	
+	def allZones
+		allzones = ["social"]
+	end
+	
+	
+	def addZoneToUser
+		addzone = User.find(current_user.id).zones.new :zone => params[:zone]
+		addzone.save
+	end
+
 	def paramPermited?
 		params[:zone] == "social"
+	end
+	
+	def zonesUserHasnt
+		zonesreturned = []
+		allZones.each do |zone|
+			if !hasUserThisZone?(zone)
+					zonesreturned << zone
+			end
+		end
+		zonesreturned
 	end
 end
