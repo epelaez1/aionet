232e811c6d07b3d42e39e3092655040783c27e51
epelaez1 <enriquepelaez97@gmail.com>
2014-08-19 10:14:08 +0200

    ;

--- app/assets/javascripts/load-on-scroll.js
+++ app/assets/javascripts/load-on-scroll.js
@@ -1,6 +1,6 @@
 $(function  () {
-	var lastTweetId = $('.tweet').last().attr('id')
+	var lastTweetId = $('.tweet').last().attr('id');
 	$.('.tweet').last().on('click', function(){
-		$.ajax('http://aionet.herokuapp.com/zones/moreTweets?last_tweet=' + lastTweetId )
-	})
-})
+		$.ajax('http://aionet.herokuapp.com/zones/moreTweets?last_tweet=' + lastTweetId );
+	});
+});
