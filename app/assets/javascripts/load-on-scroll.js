5bd2e198956d31d6a68a790d4b24942b1c35eaf4
epelaez1 <enriquepelaez97@gmail.com>
2014-08-20 17:07:18 +0200

    modified jq - scroll - down

--- app/assets/javascripts/load-on-scroll.js
+++ app/assets/javascripts/load-on-scroll.js
@@ -3,10 +3,7 @@ $(function  () {
 	$('.tweet').last().on('click', function(){
 
 		$.ajax('http://aionet.herokuapp.com/zones/moreTweets?last_tweet=' + lastTweetId ).done(function(data){
-			console.log(data);
-			for (var i = 0; i < data.length; i++) {
-				data[i].text);
-
+			for (var i = 1; i < data.length; i++) {
 				if (data[i].retweeted_status){  					
 					$(".miniboxBody").append('<div class="tweet" id=' + data[i].id_str + ' ><div class="retweeter">@' + data[i].user.screen_name + ' retweetted</div><div class="tweetImageBox"><img alt="Csakqgwk normal" class="tweetImage" src=' + data[i].retweeted_status.user.profile_image_url + '></div><div class="tweetBody"><div class="tweetAuthor">' + data[i].retweeted_status.user.name + '<div class="username">@' + data[i].retweeted_status.user.screen_name + '</div></div><div class="tweetBodyText">' + data[i].retweeted_status.text + '</div></div><div class="tweetFooter"></div></div><hr>');
 				}
