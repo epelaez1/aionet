495ab1c740f53b2967083f3028111229ebf0c7ce
epelaez1 <enriquepelaez97@gmail.com>
2014-08-20 13:31:13 +0200

    added console log

--- app/assets/javascripts/load-on-scroll.js
+++ app/assets/javascripts/load-on-scroll.js
@@ -4,6 +4,9 @@ $(function  () {
 
 		$.ajax('http://aionet.herokuapp.com/zones/moreTweets?last_tweet=' + lastTweetId ).done(function(data){
 			console.log(data);
+			for (var i = 0; i < data.length; i++) {
+				console.log(data[i].text);
+			};
 		});
 	});
 });
