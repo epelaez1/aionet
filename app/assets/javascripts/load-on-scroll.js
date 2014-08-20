e7f7c149401e2d9a197fda2f707bb0bdaabb794d
epelaez1 <enriquepelaez97@gmail.com>
2014-08-20 17:04:03 +0200

    modified jq - scroll - down

--- app/assets/javascripts/load-on-scroll.js
+++ app/assets/javascripts/load-on-scroll.js
@@ -8,51 +8,11 @@ $(function  () {
 				data[i].text);
 
 				if (data[i].retweeted_status){  					
-					$(".miniboxBody").append('<div class="tweet" id=' + data[i].id_str + ' >\
-						<div class="retweeter" >\
-							@' + data[i].user.screen_name + ' retweetted\
-						</div>\
-						<div class="tweetImageBox">\
-							<img alt="Csakqgwk normal" class="tweetImage" src=' + data[i].retweeted_status.user.profile_image_url + '>\
-						</div>\
-						<div class="tweetBody">\
-							<div class="tweetAuthor">\
-								' + data[i].retweeted_status.user.name + '\
-								<div class="username">\
-									@' + data[i].retweeted_status.user.screen_name + '\
-								</div>\
-							</div>\
-							<div class="tweetBodyText">\
-								' + data[i].retweeted_status.text + '\
-							</div>\
-						</div>\
-						<div class="tweetFooter">\
-						</div>\
-					</div>\
-					<hr>')
-					}
-				else {
-					$(".miniboxBody").append('<div class="tweet" id=' + data[i].id_str '>\
-						<div class="tweetImageBox">\
-							<img alt="Csakqgwk normal" class="tweetImage" src=' + data[i].user.profile_image_url + '>\
-						</div>\
-						<div class="tweetBody">\
-							<div class="tweetAuthor">\
-								' + data[i].user.name + '\
-								<div class="username"> @' + data[i].user.screen_name + '\
-								</div>\
-							</div>\
-							<div class="tweetBodyText">\
-								' + data[i].text + '\
-							</div>\
-						</div>\
-						<div class="tweetFooter">\
-						</div>\
-					</div>\
-					<hr>')
+					$(".miniboxBody").append('<div class="tweet" id=' + data[i].id_str + ' ><div class="retweeter">@' + data[i].user.screen_name + ' retweetted</div><div class="tweetImageBox"><img alt="Csakqgwk normal" class="tweetImage" src=' + data[i].retweeted_status.user.profile_image_url + '></div><div class="tweetBody"><div class="tweetAuthor">' + data[i].retweeted_status.user.name + '<div class="username">@' + data[i].retweeted_status.user.screen_name + '</div></div><div class="tweetBodyText">' + data[i].retweeted_status.text + '</div></div><div class="tweetFooter"></div></div><hr>');
 				}
-
-
+				else {
+					$(".miniboxBody").append('<div class="tweet" id=' + data[i].id_str '><div class="tweetImageBox"><img alt="Csakqgwk normal" class="tweetImage" src=' + data[i].user.profile_image_url + '></div><div class="tweetBody"><div class="tweetAuthor">' + data[i].user.name + '<div class="username"> @' + data[i].user.screen_name + '</div></div><div class="tweetBodyText">' + data[i].text + '</div></div><div class="tweetFooter"></div></div><hr>');
+				};
 			};
 		});
 	});
